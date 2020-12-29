ff58826b0619cb56e2f3b07219ef09b11aedcb11
KaanDoesNothing <kaanyt123@gmail.com>
2020-12-29 23:34:45 +0100

    Moving to pug(Templating engine)

--- src/website/server.js
+++ src/website/server.js
@@ -24,15 +24,15 @@ const hbsConfig = hbs.create({
     }
 });
 
-app.engine("handlebars", hbsConfig.engine);
-app.set("views", path.join(__dirname + "/views"))
-app.set("view engine", "handlebars");
+// app.engine("handlebars", hbsConfig.engine);
+// app.set("views", path.join(__dirname + "/views"))
+// app.set("view engine", "handlebars");
 
 if(!config.debugging) {
     app.set("view cache", true);
 }
-// app.set("views", path.join(__dirname + "/views_pug"));
-// app.set("view engine", "pug");
+app.set("views", path.join(__dirname + "/views"));
+app.set("view engine", "pug");
 
 app.use("/static", express.static(path.join(__dirname + "/public")));
 app.use(express.json({extended: true}));
