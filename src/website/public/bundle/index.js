46d582524baf2e199467ab05b13d1fe093ba2a3f
KaanDoesNothing <kaanyt123@gmail.com>
2020-12-30 21:16:17 +0100

    Continue later on this one

--- src/website/public/bundle/index.js
+++ src/website/public/bundle/index.js
@@ -2,8 +2,10 @@ import "bootstrap/dist/css/bootstrap.css";
 
 import bootstrap from "bootstrap/dist/js/bootstrap"
 import marked from "marked";
+import domPurify from "domPurify";
 
 window.bootstrap = bootstrap;
 window.marked = marked;
+window.domPurify = domPurify;
 
 import "../js/errorWarning";
