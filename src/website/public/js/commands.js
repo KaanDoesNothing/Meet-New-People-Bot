97dddfec1f3376f430a414ab7a4673af22b28315
KaanDoesNothing <kaanyt123@gmail.com>
2020-12-30 18:04:41 +0100

    Seperating some stuff

--- src/website/public/js/commands.js
+++ src/website/public/js/commands.js
@@ -1,21 +1,32 @@
-function search(input) {
-    input = input.toLowerCase();
+// function search(input) {
+//     input = input.toLowerCase();
     
-    $(".command").each(function() {
-        $(this).hide();
-    });
+//     $(".command").each(function() {
+//         $(this).hide();
+//     });
 
-    if(input.length < 1) {
-        $(".command").show();
-    }else {
-        commands.filter(cmd => cmd.name.startsWith(input) || cmd.name.includes(input)).map(cmd => $(`#command-${cmd.name}`).show());
+//     if(input.length < 1) {
+//         $(".command").show();
+//     }else {
+//         commands.filter(cmd => cmd.name.startsWith(input) || cmd.name.includes(input)).map(cmd => $(`#command-${cmd.name}`).show());
+//     }
+// }
+
+// $("#searchButton").on("click", function() {
+//     search($("#searchInput").val());
+// });
+
+// $("#search").on("change", (e) => {
+//     console.log(e);
+// });
+
+window._commandsState = () => {
+    return {
+        commands: window._commands,
+        categories: window._categories,
+        filter: "",
+        isTrue(name, category) {
+            return name.startsWith(this.filter.toLowerCase()) || category === this.filter;
+        }
     }
 }
-
-$("#searchButton").on("click", function() {
-    search($("#searchInput").val());
-});
-
-$("#search").on("change", (e) => {
-    console.log(e);
-});
