c53edf5df63e869d82c39ccc8c7ac53d14bbd84a
KaanDoesNothing <kaanyt123@gmail.com>
2020-12-30 20:39:24 +0100

    I'm getting tired of this.

--- src/website/public/bundle/index.js
+++ src/website/public/bundle/index.js
@@ -1,9 +1,7 @@
 import "bootstrap/dist/css/bootstrap.css";
 
-import $ from "jQuery";
 import bootstrap from "bootstrap/dist/js/bootstrap"
-import alpine from "alpine";
 
-window.$ = $;
-window.jQuery = $;
-window.alpine = alpine;
+window.bootstrap = bootstrap;
+
+import "../js/errorWarning";
