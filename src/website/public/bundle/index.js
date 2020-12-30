8972fa555940c594cd882a05fe7ddd9a4e00efe7
KaanDoesNothing <kaanyt123@gmail.com>
2020-12-30 19:30:54 +0100

    Bundling shit

---
+++ src/website/public/bundle/index.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.css";
+
+import $ from "jQuery";
+import bootstrap from "bootstrap/dist/js/bootstrap"
+import alpine from "alpine";
+
+window.$ = $;
+window.jQuery = $;
+window.alpine = alpine;
